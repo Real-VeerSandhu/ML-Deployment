0d90beb81348b1e595ff6b18c60a03ecffea9f48
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-12-27 22:08:24 -0500

    twelve

--- request.py
+++ request.py
@@ -3,4 +3,4 @@ import requests
 url = 'http://localhost:5000/predict'
 r = requests.post(url,json={'Height': 1.6})
 
-print('Prediction: ', r.json(), len(r)) # This is a comment because they are useful for all things communication, readability, and accessibility 
+print('Prediction: ', r.json(), len(r)) # This is a comment because they are useful for all things communication, readability, and accessibility there
