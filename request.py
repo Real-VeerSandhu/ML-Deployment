ac3e441eeca07d406a1e6880982a800fb30a4a37
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-01-23 23:58:04 -0500

    type

--- request.py
+++ request.py
@@ -1,6 +1,12 @@
+from urllib import request
 import requests
 
 url = 'http://localhost:5000/predict'
 r = requests.post(url,json={'Height': 1.6})
 
 print('Prediction: ', r.json(), len(r))
+
+def read(link, response, input):
+    out = request.post(link, json={'Height:': input})
+
+    print('Type:', type(out))
