0569498592c4ba85f381b1b4eec18d382767be9b
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-01-29 01:32:09 -0500

    kg

--- modelling/test.py
+++ modelling/test.py
@@ -2,6 +2,6 @@ from joblib import load
 
 model = load('modelling/linear_model.joblib')
 output = round(model.predict([[1.7]])[0][0], 1)
-prediction = f'{output} kg'
+prediction = f'{output} kg.'
 
 print(prediction)
